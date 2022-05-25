d17f3a72fe66867d09a289c3ad6db6a42fbb3d44
harperj34 <harperwiz9@outlook.com>
2022-05-25 21:19:21 +1000

    Login

--- assign2/manage.php
+++ assign2/manage.php
@@ -19,75 +19,125 @@
     ?>
     <body id = "all_quiz">
     <div class="div1">
-    <h2>Search:</h2>
-    <form method="post" action="manage.php"> <!-- This should post to the same page -->
-    <fieldset>
-        <p>
-            <label for="studentid">Student ID</label>
-                <input type = "text" name = "studentid" id = "studentid"/><br>
-            <label for="fname">First name</label>
-                <input type = "text" name = "fname" id = "fname"/><br>
-            <label for="lname">Last name</label> 
-                <input type = "text" name = "lname" id = "lname"/><br>
-            <label for="attempt_num">Attempt number</label> 
-                <input type = "text" name = "attempt_num" id = "attempt_num"/><br>
-            <label for="score">Score</label> 
-                <input type = "text" name = "score" id = "score"/><br>
-            
-        </p>
-    </fieldset>
-    <input type= "submit" value="Search"/>
-    <input type= "reset" value="Reset Form"/>
-    </form>
-    <?php error_reporting (E_ALL ^ E_NOTICE); ?>
-    <?php
-    
-        $conn = mysqli_connect($servername, $username, $password, $dbname);
-        if ($conn) {
 
-            $studentid = trim(htmlspecialchars($_POST["studentid"]));
-            $fname = trim(htmlspecialchars($_POST["fname"]));
-            $lname = trim(htmlspecialchars($_POST["lname"]));
-            $attempt_num = trim(htmlspecialchars($_POST["attempt_num"]));
-            $score = trim(htmlspecialchars($_POST["score"]));
+<?php
+    error_reporting (E_ALL ^ E_NOTICE);
 
-            echo "<p> Connection Successful!</p>";
-            $sql_table = "ATTEMPTS";
-           
-            $query = "SELECT A.AttemptID, S.StudentID, S.FirstName, S.LastName, A.Attemptdate_time, A.NumberofAttempts, A.Score FROM Attempts A INNER JOIN StudentInfo S ON A.StudentID=S.StudentID WHERE S.StudentID like '%$studentid%' AND S.FirstName LIKE '%$fname%' AND S.LastName LIKE '%$lname%' AND A.NumberofAttempts LIKE '%$attempt_num%' AND A.Score LIKE '%$score%'; ";
+    $signed_in = false;
+    $input_username = "";
+    $input_password = "";
     
-            $result = mysqli_query($conn, $query);
-            if (!$result){
-                echo "<p>Query Failed</p>";
-            }
-            else {
-                echo "<p>Select successful</p>";
-                $record = mysqli_fetch_assoc ($result);
-                if ($record) {
-                    echo "<p>Connection Successful!</p>";
-                    echo "<table border='1'>";
-                    echo "<tr><th>StudentID</th><th>FirstName</th><th>LastName</th><th>NumberofAttempts</th><th>Score</th></tr>";
-                    while ($record) {
-                        echo "<tr><td>{$record['StudentID']}</td>";
-                        echo "<td>{$record['FirstName']}</td>";
-                        echo "<td>{$record['LastName']}</td>";
-                        echo "<td>{$record['NumberofAttempts']}</td>";
-                        echo "<td>{$record['Score']}</td</tr>";
-                        $record = mysqli_fetch_assoc ($result);
+    if (!$signed_in) {
+        echo("<h2>Admin Login:</h2>");
+        echo('<form method="post" action="manage.php"> <!-- This should post to the same page -->');
+        echo("<fieldset>");
+        echo("<p>");
+            echo('<label for="username">Username: </label>');
+            echo('<input type = "text" name = "username" id = "username"/><br>');
+            echo('<label for="password">Password: </label>');
+            echo('<input type = "text" name = "password" id = "password"/><br>');          
+        echo("</p>");
+        echo("</fieldset>");
+        echo('<input type= "submit" name = "somethin" value="Login"/>');
+        echo('<input type= "reset" value="Reset Form"/>');
+        echo("</form>");
+
+        if ($_POST['somethin'] == 'Login') {
+            $conn = mysqli_connect($servername, $username, $password, $dbname);
+            if ($conn) {
+                $input_username = trim(htmlspecialchars($_POST["username"]));
+                $input_password = trim(htmlspecialchars($_POST["password"]));
+                $query = "select username, password FROM AdminInfo ORDER BY username, password";
+                $result = mysqli_query($conn, $query);
+                if ($result){
+                    $record = mysqli_fetch_assoc ($result);
+                    if ($input_username == $record['username'] and $input_password == $record['password']) {
+                        $signed_in = true;
                     }
-                    echo "</table>";
+                    else {
+                        echo("<p>Incorrect details</p>");
+                    }
                     mysqli_free_result($result);
                 }
+<<<<<<< Updated upstream
                 else {
                     echo("<p>123</p>");
                 }
             
+=======
+>>>>>>> Stashed changes
             }
-            mysqli_close($conn);
         }
-        else {
-            echo "<p>Connection Failed</p>";
+        elseif ($_POST['other'] == 'Search') {
+            $signed_in = true;
         }
+    }
+    if ($signed_in) {
+        echo("<h2>Search:</h2>");
+        echo('<form method="post" action="manage.php"> <!-- This should post to the same page -->');
+        echo("<fieldset>");
+        echo("<p>");
+            echo('<label for="studentid">Student ID</label>');
+                echo('<input type = "text" name = "studentid" id = "studentid" maxlength = "10"/><br>');
+            echo('<label for="fname">First name</label>');
+                echo('<input type = "text" name = "fname" id = "fname" maxlength = "30" pattern="[A-Za-z- ]+"/><br>');
+            echo('<label for="lname">Last name</label>');
+                echo('<input type = "text" name = "lname" id = "lname" maxlength = "30" pattern="[A-Za-z- ]+"/><br>');
+            echo('<label for="attempt_num">Attempt number</label>');
+                echo('<input type = "text" name = "attempt_num" id = "attempt_num" maxlength = "1" pattern="\d{1}"/><br>');
+            echo('<label for="score">Score</label>');
+                echo('<input type = "text" name = "score" id = "score" maxlength = "1" pattern="\d{1}"/><br>');
+        echo("</p>");
+        echo("</fieldset>");
+        echo('<input type= "submit" name = "other" value="Search"/>');
+        echo('<input type= "reset" value="Reset Form"/>');
+        echo("</form>");
+            $conn = mysqli_connect($servername, $username, $password, $dbname);
+            if ($conn) {
+
+                $studentid = trim(htmlspecialchars($_POST["studentid"]));
+                $fname = trim(htmlspecialchars($_POST["fname"]));
+                $lname = trim(htmlspecialchars($_POST["lname"]));
+                $attempt_num = trim(htmlspecialchars($_POST["attempt_num"]));
+                $score = trim(htmlspecialchars($_POST["score"]));
+                echo "<p> Connection Successful!</p>";
+                $sql_table = "ATTEMPTS";
+            
+                $query = "SELECT A.AttemptID, S.StudentID, S.FirstName, S.LastName, A.Attemptdate_time, A.NumberofAttempts, A.Score FROM Attempts A INNER JOIN StudentInfo S ON A.StudentID=S.StudentID WHERE S.StudentID like '%$studentid%' AND S.FirstName LIKE '%$fname%' AND S.LastName LIKE '%$lname%' AND A.NumberofAttempts LIKE '%$attempt_num%' AND A.Score LIKE '%$score%'; ";
+        
+                $result = mysqli_query($conn, $query);
+                if (!$result){
+                    echo "<p>Query Failed</p>";
+                }
+                else {
+                    echo "<p>Query successful</p>";
+                    $record = mysqli_fetch_assoc ($result);
+                    if ($record) {
+                        echo "<p>Matching results:</p>";
+                        echo "<table border='1'>";
+                        echo "<tr><th>Student ID</th><th>First Name</th><th>Last Name</th><th>Attempt # (Descending)</th><th>Score</th></tr>";
+                        while ($record) {
+                            echo "<tr><td>{$record['StudentID']}</td>";
+                            echo "<td>{$record['FirstName']}</td>";
+                            echo "<td>{$record['LastName']}</td>";
+                            echo "<td>{$record['NumberofAttempts']}</td>";
+                            echo "<td>{$record['Score']}</td</tr>";
+                            $record = mysqli_fetch_assoc ($result);
+                        }
+                        echo "</table>";
+                        mysqli_free_result($result);
+                    }
+                    else {
+                        echo("<p>No matching results.</p>");
+                    }
+                
+                }
+                mysqli_close($conn);
+            }
+            else {
+                echo "<p>Connection Failed</p>";
+            }
+    }
     ?>
 
 
